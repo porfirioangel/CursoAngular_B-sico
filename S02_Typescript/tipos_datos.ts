13c48cdf544a68955782acd2dc66645aa73db691
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2018-04-22 14:25:37 -0500

    Implementa ejemplo de tipos de datos de typescript

---
+++ S02_Typescript/tipos_datos.ts
@@ -0,0 +1,16 @@
+let nombre:string = "Porfirio";
+let numero:number = 21;
+let booleano:boolean = true;
+let fecha:Date = new Date();
+let cualquiera:any;
+
+cualquiera = nombre;
+cualquiera = numero;
+cualquiera = booleano;
+
+let spiderman = {
+    nombre: "Peter",
+    edad: 30
+}
+
+console.log(spiderman);
