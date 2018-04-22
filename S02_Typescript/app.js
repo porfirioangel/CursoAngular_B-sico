08e9cc9566e08f29ac161ece97be50282547d41a
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2018-04-21 23:51:26 -0500

    Crea primer ejemplo de typescript

---
+++ S02_Typescript/app.js
@@ -0,0 +1,7 @@
+function saludar(nombre) {
+    console.log('hola ' + nombre);
+}
+var wolverine = {
+    nombre: "Logan"
+};
+saludar(wolverine.nombre);
---
+++ S02_Typescript/app.ts
@@ -0,0 +1,9 @@
+function saludar(nombre:string) {
+    console.log('hola ' + nombre);
+}
+
+var wolverine = {
+    nombre: "Logan"
+}
+
+saludar(wolverine.nombre);
